1b39a843ed2bae2b1d47fa4003086373cedf04e3
Emilie Geneste <mimigeneste@gmail.com>
2026-01-23 10:05:14 +0100

    [ajout]CRUD_read_accuil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'accueil')]
-    public function index(): Response
+    public function index(ArticleRepository $repository): Response
     {
+        $articles = $repository->findAll();
         return $this->render('home/accueil.html.twig', [
-            'controller_name' => 'HomeController',
+            'articles' => $articles,
         ]);
     }
 }
