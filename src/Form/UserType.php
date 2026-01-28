4030e867c4fe5366e8c603d3cfe75b8e1c3f4f09
Emilie Geneste <mimigeneste@gmail.com>
2026-01-28 10:42:11 +0100

    [modif]responsive

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,15 +6,30 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+           ->add('email', EmailType::class,[
+                'label' => 'Email : ',
+                'attr' => [
+                    'class' => 'inscription-label'
+                ]
+            ])
             // ->add('roles')
-            ->add('password')
+            ->add('password', RepeatedType::class, [
+                    'type' => PasswordType::class,
+                    'invalid_message' => 'Les mots de passe doivent correspondre.',
+                    'required' => true,
+                    'first_options'  => ['label' => 'Mot de passe : '],
+                    'second_options' => ['label' => 'Confirmer le mot de passe : '],
+            ])
         ;
     }
 
